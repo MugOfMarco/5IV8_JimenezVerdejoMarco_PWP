907a84307b032a3b5fe4f78acafd2a6178ceaf0f
Jiménez Verdejo Marco <mjimenezv2302@alumno.ipn.mx>
2025-12-13 01:54:14 -0600

    adicion de la ultima clase

--- 14 APICRUD/Backend/routes/productroutes.js
+++ 14 APICRUD/Backend/routes/productroutes.js
@@ -1,9 +1,8 @@
-import { Router } from "express";
+import { Router } from 'express';
 import * as productController from '../controllers/productcontroller.js';
 
 const router = Router();
 
-// Ruta para crear un nuevo producto
 router.post('/products', productController.create);
 
 export default router;
